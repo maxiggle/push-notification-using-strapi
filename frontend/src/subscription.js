07f6fda6f0b36de8d9f6dc125137b6ed1162fdf5
Prn-Ice <stormprince77@gmail.com>
2022-03-27 18:01:14 +0100

    new frontend changes

--- frontend/src/subscription.js
+++ frontend/src/subscription.js
@@ -2,11 +2,17 @@
 const convertedVapidKey = urlBase64ToUint8Array(process.env.REACT_APP_PUBLIC_VAPID_KEY)
 
 function urlBase64ToUint8Array(base64String) {
-  const padding = "=".repeat((4 - base64String.length % 4) % 4)
+  const padding = "=".repeat((4 - base64String?.length % 4) % 4)
   // eslint-disable-next-line
   const base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/")
+  let rawData
+  try {
+    rawData = window?.atob(base64)
+  } catch (error) {
+    rawData = window?.atob(window.btoa("SEDRFTGYHU"))
 
-  const rawData = window.atob(base64)
+  }
+
   const outputArray = new Uint8Array(rawData.length)
 
   for (let i = 0; i < rawData.length; ++i) {
@@ -16,13 +22,13 @@ function urlBase64ToUint8Array(base64String) {
 }
 
 let jsondata = {
-  title:"Notified by Precision Ordance",
-  description:"someone buy a product",
-  icon:"https://ichef.bbci.co.uk/news/976/cpsprodpb/9A50/production/_118740593_gettyimages-1231144196.jpg"
+  title: "Notified by Precision Ordance",
+  description: "someone buy a product",
+  icon: "https://ichef.bbci.co.uk/news/976/cpsprodpb/9A50/production/_118740593_gettyimages-1231144196.jpg"
 }
 function sendSubscription(data, subscription) {
   // const { jwt: token, user }= data
-  const { jwt, ...rest }= data
+  const { jwt, ...rest } = data
   console.log(">>>", jwt, rest, subscription);
   return fetch(`${process.env.REACT_APP_API_URL}/user-notification-keys`, {
     method: 'POST',
@@ -42,22 +48,22 @@ function sendSubscription(data, subscription) {
 
 export function subscribeUser(authData) {
   if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.ready.then(function(registration) {
+    navigator.serviceWorker.ready.then(function (registration) {
       if (!registration.pushManager) {
         console.log('Push manager unavailable.')
         return
       }
 
-      registration.pushManager.getSubscription().then(function(existedSubscription) {
+      registration.pushManager.getSubscription().then(function (existedSubscription) {
         if (existedSubscription === null) {
           console.log('No subscription detected, make a request.')
           registration.pushManager.subscribe({
             applicationServerKey: convertedVapidKey,
             userVisibleOnly: true,
-          }).then(function(newSubscription) {
+          }).then(function (newSubscription) {
             console.log('New subscription added.')
             sendSubscription(authData, newSubscription)
-          }).catch(function(e) {
+          }).catch(function (e) {
             if (Notification.permission !== 'granted') {
               console.log('Permission was not granted.')
             } else {
@@ -70,7 +76,7 @@ export function subscribeUser(authData) {
         }
       })
     })
-      .catch(function(e) {
+      .catch(function (e) {
         console.error('An error ocurred during Service Worker registration.', e)
       })
   }
